3e9f1b0e5bdc888095229c12065dde50d2561d51
root <avantika.gharu97@gmail.com>
2019-06-25 10:22:21 +0530

    api

---
+++ app/Http/Resources/ArticleResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ArticleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        
+        return [
+            'id' => $this->id,
+            'app_name' => $this->title,
+            'app_body' => $this->when(true, $this->body),
+        ];
+    }
+}
